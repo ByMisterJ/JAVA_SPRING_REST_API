c070f573b15760bcdee090e1ddf8dccebf4524f5
daw2 <josearanda9000@gmail.com>
2024-02-08 11:45:57 +0100

    Eliminar

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
+//
     public static void main(String[] args) {
        ApplicationContext context = SpringApplication.run(App.class, args);
        var repo = context.getBean(ManufacturerRepository.class);
